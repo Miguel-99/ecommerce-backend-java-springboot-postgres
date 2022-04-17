4fdaf3a1a9cc5d1aa743f42563814e86ca23e302
Miguel Condori <condori.miguel99@hotmail.com>
2022-04-16 23:38:37 -0300

    Initial commit

---
+++ src/main/java/com/utng3/elbuensaborbackend/ElBuenSaborBackendApplication.java
@@ -0,0 +1,13 @@
+package com.utng3.elbuensaborbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElBuenSaborBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElBuenSaborBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/utng3/elbuensaborbackend/ElBuenSaborBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utng3.elbuensaborbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElBuenSaborBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
